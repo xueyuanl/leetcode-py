36e05af98f7c016c3b6d95c1a5787439d8ced025
hsuehyuanli <15186846+xueyuanl@users.noreply.github.com>
2020-05-24 22:46:39 +0800

    update

--- algorithm.md
+++ algorithm.md
@@ -5,15 +5,19 @@
 * 22\. Generate Parentheses
 
 # String
+* [451\. Sort Characters By Frequency](problems/sort_characters_by_frequency.py) [map, sorted]
 * 1108\. Defanging an IP Address
 ### Sliding window
-* [567\. Permutation in String](problems/permutation_in_string.py)(Hash)
+* [567\. Permutation in String](problems/permutation_in_string.py) [hash]
+
 
 # Dynamic Programming
 * 62\. Unique Paths
 * 64\. Minimum Path Sum
 * 337\. House Robber III
 * 96\. Unique Binary Search Trees
+
+# Tree
 
 # Math
 
--- problems/sort_characters_by_frequency.py
+++ problems/sort_characters_by_frequency.py
@@ -4,3 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        if not s:
+            return ''
+
+        m = {}
+
+        l = len(s)
+        for i in range(l):
+            ch = s[i]
+            if ch in m:
+                m[ch] += 1
+            else:
+                m[ch] = 1
+
+        cf = []  # character_frequency
+        for key in m:
+            cf.append((m[key], key))
+
+        sorted_cf = sorted(cf, reverse=True)
+
+        res = []
+
+        for i in sorted_cf:
+            res.append(i[1] * i[0])
+
+        return ''.join(res)
