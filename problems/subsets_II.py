2fc9fd53fdfa3c012002fab036fc4d7164bd2ecc
tutu <15186846+xueyuanl@users.noreply.github.com>
2020-06-04 08:29:18 -0700

    update

--- problems/subsets_II.py
+++ problems/subsets_II.py
@@ -1,6 +1,3 @@
-import copy
-
-
 class Solution(object):
     def subsetsWithDup(self, nums):
         """
@@ -9,14 +6,17 @@ class Solution(object):
         """
 
         self.count = len(nums)
-
+        self.sorted_nums = sorted(nums)  # sorted first,
         self.res = []
-        self.backtracking([])
+        self.backtracking([], 0)
         return self.res
 
-    def backtracking(self, sub_set , index ):
+    def backtracking(self, sub_set, index):
         self.res.append(sub_set[:])
 
-        for i in range(index, self.count)
-
-
+        for i in range(index, self.count):
+            if i > index and self.sorted_nums[i] == self.sorted_nums[i - 1]:
+                continue
+            sub_set.append(self.sorted_nums[i])
+            self.backtracking(sub_set, i + 1)
+            sub_set.pop()
