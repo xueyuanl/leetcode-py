62e52237444dbe396517007b71f165143e57f711
tutu <15186846+xueyuanl@users.noreply.github.com>
2020-06-06 08:01:11 -0700

    update

--- problems/combinations.py
+++ problems/combinations.py
@@ -5,3 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        self.res = []
+        self.n = n
+        self.k = k
+        self.dfs([], 0)
+        return self.res
+
+    def dfs(self, case, index):
+        if len(case) == self.k:
+            self.res.append(case[:])
+            return
+
+        for i in range(index, self.n):
+            case.append(i + 1)
+            self.dfs(case, i + 1)
+            case.pop()
+
+
+
+
+
